8be75ffa6007022c43641d855dabb0d816b6fdef
qxz23 <3119476850@qq.com>
2022-10-24 22:41:54 +0800

    Site updated: 2022-10-24 22:41:53

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/a0ca7bc4/","posts/4257a649/","posts/4a17b156/","posts/4787801/","posts/6cb5a254/","posts/be82d226/","posts/73626faf/","posts/7ec7be7a/","posts/9daba997/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["posts/a0ca7bc4/","posts/4257a649/","posts/4787801/","posts/be82d226/","posts/7ec7be7a/","posts/4a17b156/","posts/73626faf/","posts/9daba997/","posts/6cb5a254/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
